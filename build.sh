c010c5f4b65efb63479cbfbe1ac7a3d33dd24932
alturismo <alturismo@gmail.com>
2024-10-15 10:08:41 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REPOSITORY="$1"
-
-VERSION="$2"
-
-docker build --rm --target nomariadb -t "$REPOSITORY"/guacamole:"$VERSION"-nomariadb .
-
-docker build --rm -t "$REPOSITORY"/guacamole:"$VERSION" .
